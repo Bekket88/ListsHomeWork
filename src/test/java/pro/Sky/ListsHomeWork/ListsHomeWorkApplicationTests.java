9bb4b9a86db85d5c33445f3fd4a6e10e6358a92e
Vladimir Popov <Bekket88@gmail.com>
2022-08-16 17:23:54 +0300

    First commit

---
+++ src/test/java/pro/Sky/ListsHomeWork/ListsHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.Sky.ListsHomeWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListsHomeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
